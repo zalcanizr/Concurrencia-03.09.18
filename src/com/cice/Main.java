921ec9e96bb1640dc513e1e242e5fa09cb7ed12f
cice <cice@A8POVMac10.local>
2018-09-05 19:18:14 +0200

    ultimos cambios

--- src/com/cice/Main.java
+++ src/com/cice/Main.java
@@ -1,11 +1,37 @@
 package com.cice;
 
+import com.cice.hilos.HiloPropio;
+
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
         //
+        long inicio = System.currentTimeMillis();
+
+        HiloPropio hilo = new HiloPropio();
+
+        hiloNuevo();
+
+        //hilo.start();
+        for (int i = 0; i < 10000; i++){
+            System.out.println("Vuelta del main: " + i);
+        }
+
+        long fin = System.currentTimeMillis();
+
+        System.out.println("Tiempo total de ejecucion: "+(fin-inicio));
 
     }
+
+    private static void hiloNuevo() {
+        for (int i = 0; i < 10000; i++){
+            System.out.println("Vuelta del hilo propio: " + i);
+        }
+    }
+
+
 }
+
--- src/com/cice/hilos/HiloPropio.java
+++ src/com/cice/hilos/HiloPropio.java
@@ -9,8 +9,8 @@ public class HiloPropio extends Thread  {
     @Override
     public void run() {
 
-        for (int i=0;i<100;i++){
-            
+        for (int i = 0; i < 10000; i++){
+            System.out.println("Vuelta del hilo propio: " + i);
         }
     }
 }
